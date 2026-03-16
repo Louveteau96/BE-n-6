1f88114a0f37b1e31e1f75d37d199088d97dc35d
Louveteau96 <elijahpeyrat@gmail.com>
2026-03-16 14:19:21 +0100

    Ajout test Elijah
    Mon début de code test pour la connexion

---
+++ Test Elijah.py
@@ -0,0 +1,33 @@
+import serial
+
+#Connexion port série
+def connexion(port_serie):
+    port_serie = "Com3"
+    ser = serial.Serial(port_serie,baudrate=9600,timeout=10)
+    return ser
+
+
+#Initialistation de l'appareil
+def initialisation(ser):
+    #set mode remote pour pouvoir interagir avec l'appareil
+    ser.write("set mode remote")
+    reponse = ser.readline()
+    if(reponse != "set mode remote ok"):
+        print("Initialisation échouée remote")
+        return
+    
+    #gas unit en ppb
+    ser.write("set gas unit ppb")
+    reponse = ser.readline()
+    if(reponse != "set gas unit ppb ok"):
+        print("Initialisation échouée ppb")
+        return
+    return
+
+#Envoi et réception des commandes
+def recuperation_donnee(ser):
+    ser.write("lrec 1 1") #l'instrument envoi 1 enregistrement
+    #Ce qu'on recoit
+    #lrec 1 1 heure date flag o3 hio3 cellA cellB benchT lampT o3lamp flowA, flowB, pression
+
+
